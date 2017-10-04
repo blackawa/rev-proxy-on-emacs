36dd91449edacaf601cf860b73cbfa1a5abc676d
blackawa <3104kurokawa@gmail.com>
2017-10-04 19:48:52 +0900

    :seedling:

---
+++ proxy.el
@@ -0,0 +1,47 @@
+(require 'web)
+
+(setq counter (make-hash-table :test 'equal))
+(setq blacklist '(""))
+
+(defun put-access-time (ip)
+  (let ((access-times (gethash ip counter)))
+    (puthash ip
+             (push (truncate (float-time)) access-times)
+             counter)))
+
+(defun edos? (ip)
+  (let* ((access-times (put-access-time ip))
+         (length (length access-times))
+         (latest (first access-times))
+         (oldest (or (nth 9 access-times) (last access-times)))
+         (attacked? (and (> length 10) (> 5 (- latest oldest))))
+         (blacklisted? (member ip blacklist)))
+    (when (and attacked? (not blacklisted?)) (push ip blacklist))
+    (or attacked? blacklisted?)))
+
+(defun render-service-unavailable (httpcon)
+  (elnode-http-start httpcon 503 '("Content-Type" . "text/html"))
+  (elnode-http-return httpcon "<html><h1>Service unavailable.</h1></html>"))
+
+(defun forward-to-backend (httpcon method path)
+  (web-http-call
+   method
+   (lambda (con header data)
+     (elnode-http-start httpcon (gethash 'status-code header) '("Content-Type" . "text/html"))
+     (elnode-http-return httpcon (format "%S" data)))
+   :mode 'stream
+   :url (format "http://localhost:3000%s" path)))
+
+(defun proxy-handler (httpcon)
+  (let ((method (elnode-http-method httpcon))
+        (path (elnode-http-pathinfo httpcon))
+        (remote-addr (replace-regexp-in-string ":[0-9]+" "" (elnode-remote-ipaddr httpcon))))
+    (if (edos? remote-addr) (render-service-unavailable httpcon) (forward-to-backend httpcon method path))))
+
+(elnode-start 'proxy-handler :port 8000 :host "localhost")
+
+(web-http-call
+ "GET"
+ (lambda (con header data)
+   (message "%s" (gethash 'status-code header)))
+ :url "http://localhost:3000")
