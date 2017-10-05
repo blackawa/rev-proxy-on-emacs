702787d7d1f476350c5f20fc57f52277feb91b65
blackawa <3104kurokawa@gmail.com>
2017-10-05 14:20:06 +0900

    minimum implementation

--- proxy.el
+++ proxy.el
@@ -25,13 +25,17 @@
   (elnode-http-return httpcon "<html><h1>Service unavailable.</h1></html>"))
 
 (defun forward-to-backend (httpcon method path)
-  (web-http-call
-   method
-   (lambda (con header data)
-     (elnode-http-start httpcon (gethash 'status-code header) '("Content-Type" . "text/html"))
-     (elnode-http-return httpcon (format "%s" data)))
-   :mode 'stream
-   :url (format "http://localhost:3000%s" path)))
+  (deferred:sync!
+    (deferred:$
+      (deferred:url-retrieve (format "http://localhost:3000%s" path))
+      (deferred:nextc it
+        (lambda (buf)
+          (let ((res (with-current-buffer buf (buffer-string))))
+            (kill-buffer buf)
+            (if (string-suffix-p ".css" path)
+                (elnode-http-start httpcon 200 '("Content-Type" . "text/css"))
+              (elnode-http-start httpcon 200 '("Content-Type" . "text/html")))
+            (elnode-http-return httpcon res)))))))
 
 (defun proxy-handler (httpcon)
   (let ((method (elnode-http-method httpcon))
@@ -40,19 +44,3 @@
     (if (edos? remote-addr) (render-service-unavailable httpcon) (forward-to-backend httpcon method path))))
 
 (elnode-start 'proxy-handler :port 8000 :host "localhost")
-
-(let ((process
-       (web-http-call
-        "GET"
-        (lambda (con header data)
-          data)
-        :url "http://localhost:3000")))
-  (call-process process))
-
-(deferred:sync!
-  (deferred:$
-    (deferred:url-retrieve "http://localhost:3000")
-    (deferred:nextc it
-      (lambda (buf)
-        (with-current-buffer buf (buffer-string))
-        (kill-buffer buf)))))
